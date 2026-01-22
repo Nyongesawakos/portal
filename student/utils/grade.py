916df75df7a42fa2c08ef8fc11fe5827eb12bf34
Nyongesawakos <108975266+Nyongesawakos@users.noreply.github.com>
2026-01-22 14:18:37 +0300

    Add files via upload

---
+++ student/utils/grade.py
@@ -0,0 +1,10 @@
+def calculate_grade(marks):
+    if marks >= 70:
+        return "A"
+    elif marks >= 60:
+        return "B"
+    elif marks >= 50:
+        return "C"
+    elif marks >= 40:
+        return "D"
+    return "F"
