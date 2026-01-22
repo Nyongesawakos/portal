219fff210699a72ade9c6dce6f76ef1e482d328b
Nyongesawakos <108975266+Nyongesawakos@users.noreply.github.com>
2026-01-22 14:19:31 +0300

    Add files via upload

---
+++ portal/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Student(models.Model):
+    reg_no = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    course = models.CharField(max_length=100)
+    fee_paid = models.CharField(max_length=100, blank=True)
+    arrears = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.reg_no
+class CourseUnit(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    title = models.CharField(max_length=100)
+    credit_units = models.IntegerField()
+
+    def __str__(self):
+        return self.code
+class Result(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    unit = models.ForeignKey(CourseUnit, on_delete=models.CASCADE)
+    marks = models.IntegerField()
+    grade = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f"{self.student} - {self.unit}"
